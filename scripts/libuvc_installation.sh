7aafe1d7592d04f5b3592ebc698b16d4ab45357d
saskakarac <saska.karac@gmail.com>
2025-03-29 14:41:31 +0100

    Update libuvc_installation.sh

--- scripts/libuvc_installation.sh
+++ scripts/libuvc_installation.sh
@@ -34,16 +34,15 @@ echo Installing Librealsense-required dev packages
 sudo apt-get install git cmake libssl-dev freeglut3-dev libusb-1.0-0-dev pkg-config libgtk-3-dev unzip -y
 rm -f ./master.zip
 
-wget https://github.com/IntelRealSense/librealsense/archive/master.zip
-unzip ./master.zip -d .
-cd ./librealsense-master
+wget https://github.com/saskakarac/librealsense.git
+cd ./librealsense
 
 echo Install udev-rules
 sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/ 
 sudo cp config/99-realsense-d4xx-mipi-dfu.rules /etc/udev/rules.d/
 sudo udevadm control --reload-rules && sudo udevadm trigger 
 mkdir build && cd build
-cmake ../ -DFORCE_LIBUVC=true -DCMAKE_BUILD_TYPE=release
+cmake ../ -DFORCE_LIBUVC=true -DCMAKE_BUILD_TYPE=release -DBUILD_NETWORK_DEVICE=true -DFORCE_RSUSB_BACKEND=true
 make -j2
 sudo make install
 echo -e "\e[92m\n\e[1mLibrealsense script completed.\n\e[0m"
